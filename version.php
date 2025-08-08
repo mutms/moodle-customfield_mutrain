53e0aaed18adb2dfd55c74641607338008eef24f
Petr Skoda <petr@mutms.org>
2025-08-08 10:29:41 +0300

    Prepare mu-5.0.2-01 release

--- version.php
+++ version.php
@@ -29,9 +29,9 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'customfield_mutrain';
-$plugin->version = 2025063050;
+$plugin->version = 2025080950;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 500];
 
-$plugin->release = 'mu-5.0.1-01+';
+$plugin->release = 'mu-5.0.2-01';
